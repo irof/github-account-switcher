9f97cb3c198268e7d4526e574ca11d1718b62bfb
irof <irof@hogedriven.net>
2018-09-08 10:01:58 +0900

    eslintはeslintコマンドでやることにする

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-
-gulp.task('default', () => {
-  gulp.src(['src/**/*.js'])
-    .pipe(eslint({"fix": true}))
-    .pipe(eslint.format());
-});
-
