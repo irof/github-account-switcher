6dd07576d9956ce27b2f9bdcf1e8256bbafcbed0
irof <irof@hogedriven.net>
2018-09-08 10:03:49 +0900

    eslint --fix

--- src/background.js
+++ src/background.js
@@ -1,14 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-      chrome.declarativeContent.onPageChanged.addRules([
-        {
-          conditions: [
-            new chrome.declarativeContent.PageStateMatcher({
-              pageUrl: { hostEquals: 'github.com' }
-            })
-          ],
-          actions: [ new chrome.declarativeContent.ShowPageAction() ]
-        }
-      ]);
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [
+                    new chrome.declarativeContent.PageStateMatcher({
+                        pageUrl: { hostEquals: "github.com" }
+                    })
+                ],
+                actions: [ new chrome.declarativeContent.ShowPageAction() ]
+            }
+        ]);
     });
-  });
+});
